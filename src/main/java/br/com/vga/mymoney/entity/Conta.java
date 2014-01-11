8482cfa480e23968ec3bb67055b05ffc87d51d6f
Investigador Bandeira <investigador.bandeira@gmail.com>
2014-01-11 00:28:37 -0300

    Adicionando classes iniciais.

---
+++ src/main/java/br/com/vga/mymoney/entity/Conta.java
@@ -0,0 +1,11 @@
+package br.com.vga.mymoney.entity;
+
+import java.math.BigDecimal;
+
+public class Conta {
+
+    private Long id;
+
+    private String nome;
+    private BigDecimal saldoInicial;
+}
