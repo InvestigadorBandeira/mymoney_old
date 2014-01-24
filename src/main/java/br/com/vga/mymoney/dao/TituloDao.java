53b1c9bf40bdcb1ff3c8d9202a343841a9fa2c9a
Vinicius Lima <vinicius.lima@gm.ifbaiano.edu.br>
2014-01-24 09:52:44 -0300

    Correção no TituloDao.

--- src/main/java/br/com/vga/mymoney/dao/TituloDao.java
+++ src/main/java/br/com/vga/mymoney/dao/TituloDao.java
@@ -2,9 +2,9 @@ package br.com.vga.mymoney.dao;
 
 import javax.persistence.EntityManager;
 
-import br.com.vga.mymoney.entity.Parcela;
+import br.com.vga.mymoney.entity.Titulo;
 
-public class TituloDao extends AbstractDao<Parcela> {
+public class TituloDao extends AbstractDao<Titulo> {
 
     public TituloDao(EntityManager em) {
 	super(em);
