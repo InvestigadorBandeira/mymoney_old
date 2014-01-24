f88b85b940d8e4fbefc169b98f1a2e60c484719b
Vinicius Lima <vinicius.lima@gm.ifbaiano.edu.br>
2014-01-24 09:44:00 -0300

    Criação das entidades: título e parcela.

---
+++ src/main/java/br/com/vga/mymoney/entity/Parcela.java
@@ -0,0 +1,107 @@
+package br.com.vga.mymoney.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Parcela implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Temporal(TemporalType.DATE)
+    private Calendar dataVencimento;
+
+    private BigDecimal valor;
+
+    private SubCategoria subCategoria;
+
+    private String observacao;
+
+    @ManyToOne
+    private Titulo titulo;
+
+    public Long getId() {
+	return id;
+    }
+
+    public void setId(Long id) {
+	this.id = id;
+    }
+
+    public Calendar getDataVencimento() {
+	return dataVencimento;
+    }
+
+    public void setDataVencimento(Calendar dataVencimento) {
+	this.dataVencimento = dataVencimento;
+    }
+
+    public BigDecimal getValor() {
+	return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+	this.valor = valor;
+    }
+
+    public SubCategoria getSubCategoria() {
+	return subCategoria;
+    }
+
+    public void setSubCategoria(SubCategoria subCategoria) {
+	this.subCategoria = subCategoria;
+    }
+
+    public String getObservacao() {
+	return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+	this.observacao = observacao;
+    }
+
+    public Titulo getTitulo() {
+	return titulo;
+    }
+
+    public void setTitulo(Titulo titulo) {
+	this.titulo = titulo;
+    }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	Parcela other = (Parcela) obj;
+	if (id == null) {
+	    if (other.id != null)
+		return false;
+	} else if (!id.equals(other.id))
+	    return false;
+	return true;
+    }
+
+}
