09d120c6f8b1442e28d071696643690c74f4062c
Vinicius Lima <vinicius.lima@gm.ifbaiano.edu.br>
2014-01-31 14:01:48 -0300

    Tabela Parela.

---
+++ src/main/java/br/com/vga/mymoney/view/tables/ParcelaTable.java
@@ -0,0 +1,32 @@
+package br.com.vga.mymoney.view.tables;
+
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+
+import br.com.vga.mymoney.view.tables.models.ParcelaTableModel;
+
+public class ParcelaTable extends JTable {
+    private static final long serialVersionUID = 1L;
+
+    ParcelaTableModel model;
+
+    public ParcelaTable(ParcelaTableModel tableModel) {
+	this.model = tableModel;
+	configura();
+    }
+
+    private void configura() {
+	setModel(model);
+	setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+	getColumnModel().getColumn(model.DATA_VENCIMENTO).setResizable(false);
+	getColumnModel().getColumn(model.VALOR).setResizable(false);
+	getColumnModel().getColumn(model.SUBCATEGORIA).setResizable(false);
+	getColumnModel().getColumn(model.OBSERVACAO).setResizable(false);
+
+	getColumnModel().getColumn(model.DATA_VENCIMENTO).setPreferredWidth(50);
+	getColumnModel().getColumn(model.VALOR).setPreferredWidth(50);
+	getColumnModel().getColumn(model.SUBCATEGORIA).setPreferredWidth(100);
+	getColumnModel().getColumn(model.OBSERVACAO).setPreferredWidth(250);
+    }
+}
