4c7b96883713fd7ba3f2c2c3599f0fff568d5ef6
Investigador Bandeira <investigador.bandeira@gmail.com>
2014-02-04 17:18:05 -0300

    Formatação de valor.

--- src/main/java/br/com/vga/mymoney/util/Formatador.java
+++ src/main/java/br/com/vga/mymoney/util/Formatador.java
@@ -1,5 +1,7 @@
 package br.com.vga.mymoney.util;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -8,6 +10,11 @@ public class Formatador {
     public static String dataTexto(Calendar data) {
 	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	return data != null ? sdf.format(data.getTime()) : "";
+    }
+
+    public static String valorTexto(BigDecimal valor) {
+	DecimalFormat df = new DecimalFormat("#,##0.00;-#,##0.00");
+	return valor != null ? df.format(valor) : "0,00";
     }
 
 }
