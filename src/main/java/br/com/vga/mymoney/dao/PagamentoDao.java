bc7d4c8d0db1d9a151597abe6bcd5e947e418e77
Investigador Bandeira <Investigador.bandeira@gmail.com>
2014-02-12 09:44:52 -0300

    Adição do método totalPgtoPorConta.

--- src/main/java/br/com/vga/mymoney/dao/PagamentoDao.java
+++ src/main/java/br/com/vga/mymoney/dao/PagamentoDao.java
@@ -1,7 +1,11 @@
 package br.com.vga.mymoney.dao;
 
-import javax.persistence.EntityManager;
+import java.math.BigDecimal;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import br.com.vga.mymoney.entity.Conta;
 import br.com.vga.mymoney.entity.Pagamento;
 
 public class PagamentoDao extends AbstractDao<Pagamento> {
@@ -10,4 +14,17 @@ public class PagamentoDao extends AbstractDao<Pagamento> {
 	super(em);
     }
 
+    public BigDecimal totalPgtoPorConta(Conta conta) {
+
+	String jpql = "SELECT SUM(p.valorTotal) FROM Pagamento p WHERE"
+		+ " p.conta = :conta AND p.data <= CURRENT_DATE";
+
+	TypedQuery<BigDecimal> query = em.createQuery(jpql, BigDecimal.class);
+	query.setParameter("conta", conta);
+
+	BigDecimal total = query.getSingleResult();
+
+	return total != null ? total : BigDecimal.ZERO;
+
+    }
 }
